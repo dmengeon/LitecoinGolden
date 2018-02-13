0c3a62ed4d37475bfe3678ba8a97ea30cfd668e1
dmengeon <dmengeon@hotmail.com>
2018-02-12 23:21:32 -0200

    Initial commit

---
+++ Block Information.sol
@@ -0,0 +1,17 @@
+  Height:	    4972780 
+  TimeStamp:	18 days 59 mins ago (Jan-25-2018 11:51:56 PM +UTC)
+  Transactions:	103 transactions and 18 contract internal transactions in this block
+  Hash:	        0x2115dec2989ee74f66add694731980be499b4ecd9e4bd0413ec31bb89d31d335
+  Parent Hash:	0xbed7c176b80e74f2e373cdf703a4046794c10d3578a544de789a92283cd2595f
+  Sha3Uncles:	0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347
+  Mined By:	    0x5a0b54d5dc17e0aadc383d2db43b0a0d3e029c4c (ethfans.org_2) in 10 secs
+  Difficulty:	2,450,589,363,017,003
+  Total Difficulty:	2,216,248,816,306,710,305,803
+  Size:	        17531 bytes
+  Gas Used:	    4,319,028 (54.04%)
+  Gas Limit:	7,992,222
+  Nonce:	    0x88c017fae023b6a0ca
+  Block Reward:	3.112669914344302939 Ether (3 + 0.112669914344302939)
+  Uncles Reward:	0
+  Extra Data:	ETH.ETHFANS.ORG-85D9466B (Hex:0x4554482e45544846414e532e4f52472d3835443934363642)
+  LitecoinGolden LTGO 
---
+++ Read Contract Information.sol
@@ -0,0 +1,15 @@
+1.  name    LitecoinGolden string
+2.  totalSupply    5250000 uint256
+3.  decimals    0 uint8
+4.  version    H0.1 string
+5.  balanceOf   
+   _owner (address)
+   Query
+ balance uint256
+6.  symbol    LTGO string
+7.  allowance   
+   _owner (address)
+, 
+   _spender (address)
+   Query
+ remaining uint256
